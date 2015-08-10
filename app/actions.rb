c48eb0f2fa609d57fac4974a239436bbbde51a1d
KludgeMaster <yiryans@gmai.com>
2015-08-10 01:28:29 +0000

    works but i donno why

--- app/actions.rb
+++ app/actions.rb
@@ -24,3 +24,17 @@ get '/messages' do
   @messages = Message.all
   erb :'messages/index'
 end
+
+get '/messages/new' do
+  erb :'messages/new'  
+end
+
+post '/messages' do
+  @message = Message.new(
+    title: params[:title],
+    content: params[:content],
+    author:  params[:author]
+  )
+  @message.save
+  redirect '/messages'
+end
